049b214a2a09ca99dd17546cd4bb109086e3391b
Justin Le <justin@jle.im>
2014-06-29 03:27:31 -0700

    many Event primitives...i wonder which ones I'll eventually need or keep.

--- src/Control/Auto/Event.hs
+++ src/Control/Auto/Event.hs
@@ -49,8 +49,15 @@ never = pure NoEvent
 now :: Monad m => Auto m a (Event a)
 now = mkState f False
   where
-    f x False = (Event x, True)
     f _ True  = (NoEvent, True)
+    f x False = (Event x, True)
+
+inE :: Monad m => Int -> Auto m a (Event a)
+inE n = mkState f (n, False)
+  where
+    f _ (_, True)              = (NoEvent, (0  , True ))
+    f x (i, False) | i <= 0    = (Event x, (0  , True ))
+                   | otherwise = (NoEvent, (i-1, False))
 
 every :: Monad m => Int -> Auto m a (Event a)
 every n | n <= 0    = error "every: Non-positive interval"
@@ -76,3 +83,29 @@ eachAt_ n xs | n <= 0    = error "eachAt: Non-positive interval"
     f (y:ys, 1) = (Event y, (ys, n  ))
     f (_   , 0) = (NoEvent, ([], 0  ))
     f (ys  , i) = (NoEvent, (ys, i-1))
+
+filterE :: Monad m => (a -> Bool) -> Auto m (Event a) (Event a)
+filterE p = mkFunc $ \x -> case x of
+                             Event x' | p x' -> x
+                             _               -> NoEvent
+
+once :: Monad m => Auto m (Event a) (Event a)
+once = mkState f False
+  where
+    f _           True = (NoEvent, True )
+    f e@(Event _) _    = (e,       True )
+    f _           _    = (NoEvent, False)
+
+takeE :: Monad m => Int -> Auto m (Event a) (Event a)
+takeE = mkState f
+  where
+    f _ 0           = (NoEvent, 0  )
+    f e@(Event _) i = (e      , i-1)
+    f _           i = (NoEvent, i  )
+
+dropE :: Monad m => Int -> Auto m (Event a) (Event a)
+dropE = mkState f
+  where
+    f x         0 = (x      , 0  )
+    f (Event _) i = (NoEvent, i-1)
+    f _         i = (NoEvent, i  )
