60bb996ae0e81e0217601761eab5ac7292a86d38
Justin Le <justin@jle.im>
2014-07-01 15:59:35 -0700

    various independently implemented muxes

--- src/Control/Auto/Collection.hs
+++ src/Control/Auto/Collection.hs
@@ -34,17 +34,77 @@ dynZip_ x0 = go []
                          let (ys, as') = unzip [ (y, a) | (Output (Just y) a) <- res ]
                          return (Output ys (go as'))
 
-mux :: forall m a b k. (Binary k, Ord k, Monad m) => (k -> Auto m a b) -> Auto m (k, a) b
+mux :: forall m a b k. (Binary k, Ord k, Monad m)
+    => (k -> Auto m a b)
+    -> Auto m (k, a) b
 mux f = go mempty
   where
     go :: Map k (Auto m a b) -> Auto m (k, a) b
-    go as = mkAutoM l (s as) $ \(k, x) -> do
-                                 let a = M.findWithDefault (f k) k as
-                                 Output y a' <- stepAuto a x
-                                 let as' = M.insert k a' as
-                                 return (Output y (go as'))
+    go as = _mkMux f go as (goF as)
+    goF = _muxF f go
+
+mux_ :: forall m a b k. (Ord k, Monad m) => (k -> Auto m a b) -> Auto m (k, a) b
+mux_ f = go mempty
+  where
+    go :: Map k (Auto m a b) -> Auto m (k, a) b
+    go as = mkAutoM_ (goF as)
+    goF = _muxF f go
+
+_muxF :: (Ord k, Monad m)
+      => (k -> Auto m a b)
+      -> (Map k (Auto m a b) -> Auto m (k, a) b)
+      -> Map k (Auto m a b)
+      -> (k, a)
+      -> m (Output m (k, a) b)
+_muxF f go as (k, x) = do
+    let a = M.findWithDefault (f k) k as
+    Output y a' <- stepAuto a x
+    let as' = M.insert k a' as
+    return (Output y (go as'))
+
+muxI :: forall m a b k. (Binary k, Ord k, Monad m)
+     => (k -> Auto m a (Maybe b))
+     -> Auto m (k, a) (Maybe b)
+muxI f = go mempty
+  where
+    go :: Map k (Auto m a (Maybe b)) -> Auto m (k, a) (Maybe b)
+    go as = _mkMux f go as (goF as)
+    goF = _muxIF f go
+
+
+muxI_ :: forall m a b k. (Binary k, Ord k, Monad m)
+      => (k -> Auto m a (Maybe b))
+      -> Auto m (k, a) (Maybe b)
+muxI_ f = go mempty
+  where
+    go :: Map k (Auto m a (Maybe b)) -> Auto m (k, a) (Maybe b)
+    go as = mkAutoM_ (goF as)
+    goF = _muxIF f go
+
+_muxIF :: (Ord k, Monad m)
+       => (k -> Auto m a (Maybe b))
+       -> (Map k (Auto m a (Maybe b)) -> Auto m (k, a) (Maybe b))
+       -> Map k (Auto m a (Maybe b))
+       -> (k, a)
+       -> m (Output m (k, a) (Maybe b))
+_muxIF f go as (k, x) = do
+    let a = M.findWithDefault (f k) k as
+    Output y a' <- stepAuto a x
+    let as' = case y of
+                Just _  -> M.insert k a' as
+                Nothing -> M.delete k as
+    return (Output y (go as'))
+
+_mkMux :: (Binary k, Ord k, Monad m)
+       => (k -> Auto m a b)
+       -> (Map k (Auto m a b) -> Auto m (k, a) b)
+       -> Map k (Auto m a b)
+       -> ((k, a) -> m (Output m (k, a) b))
+       -> Auto m (k, a) b
+_mkMux f go as = mkAutoM l s
+  where
     l = do
       ks <- get
       let as' = M.fromList (map (id &&& f) ks)
       go <$> mapM loadAuto as'
-    s as = put (M.keys as) *> mapM_ saveAuto as
+    s = put (M.keys as) *> mapM_ saveAuto as
