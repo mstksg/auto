aac370a6051774477690710650b802916b5ee32f
Justin Le <justin@jle.im>
2015-04-28 15:05:37 -0700

    comitting the MaybeT combinators before they are deleted

--- src/Control/Auto/Effects.hs
+++ src/Control/Auto/Effects.hs
@@ -61,6 +61,9 @@ module Control.Auto.Effects (
   , stateA
   , accumA
 
+  -- , runMaybeA
+  -- , maybeA
+
   -- ** 'Traversable'
   , runTraversableA
 
@@ -919,6 +922,24 @@ runTraversableA = go . return
                               y  = liftM fst o
                               a' = liftM snd o
                           in  (y, go a')
+
+
+-- maybeA :: Monad m => Auto m a (Maybe b) -> Auto (MaybeT m) a b
+-- maybeA a = mkAutoM (maybeA <$> resumeAuto a)
+--                    (saveAuto a)
+--                  $ \x -> do
+--                      (y, a') <- lift $ stepAuto a x
+--                      y' <- maybe mzero return y
+--                      return (y', maybeA a')
+
+-- runMaybeA :: Monad m => Auto (MaybeT m) a b -> Auto m a (Maybe b)
+-- runMaybeA a = mkAutoM (runMaybeA <$> resumeAuto a)
+--                       (saveAuto a)
+--                     $ \x -> do
+--                         mya' <- runMaybeT . stepAuto a $ x
+--                         return $ case mya' of
+--                           Nothing      -> (Nothing, runMaybeA a )
+--                           Just (y, a') -> (Just y , runMaybeA a')
 
 -- | Wraps a "try" over an underlying 'IO' monad; if the Auto encounters a
 -- runtime exception while trying to "step" itself, it'll output a 'Left'
